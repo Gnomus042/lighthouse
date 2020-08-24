8fea948175368577f08ff0e05b2ab39af1c132b5
Anastasiia Byvsheva <anabyvsheva@google.com>
2020-08-24 18:24:25 +0300

    Local shex shapes used for validation, added raw version of raw shapes, added support of @graph validation

--- lighthouse-core/lib/sd-validation/schema-validator.js
+++ lighthouse-core/lib/sd-validation/schema-validator.js
@@ -5,25 +5,32 @@
  */
 'use strict';
 
+const jsonld = require('jsonld');
+const ParserJsonld = require('@rdfjs/parser-jsonld');
+const toReadableStream = require('to-readable-stream');
+const rdf = require('rdf-ext');
 const shexValidator = require('./helpers/shexValidator.js');
 const shaclValidator = require('./helpers/shaclValidator.js');
 const context = require('./assets/context.json');
+const { da } = require('../i18n/locales.js');
 
 // TODO will need to remove these fields
 const defaultId = 'http://example.org/recipe';
-const supportedServices = ['Google', 'Bing', 'Pinterest', 'Yandex'];
+const supportedServices = [''];
 const linkToShexShapes = 'https://gnomus042.com/shex/shapes';
 
-/** @typedef {{property: string|null, severity: string, message: string, url: string|null, description: string|null, service: string|null}} StructuredDataReport */
+/** @typedef {{property: string|null, severity: string, message: string, url: string|null, description: string|null, services: Array<string>|null}} StructuredDataReport */
 
 /**
  * Adds context to data and sets dummy id for simplicity
  * @param {string} inputText JSON-LD object in string format
- * @returns {string}
+ * @returns {*}
  */
 function prepareInput(inputText) {
   const data = JSON.parse(inputText);
-  data['@id'] = defaultId;
+  if (!data['@id']) {
+    data['@id'] = defaultId;
+  }
   data['@context'] = context;
   return JSON.stringify(data);
 }
@@ -39,19 +46,63 @@ function findIntersection(shexReport, shaclReport) {
   return shexReport.filter(a => shaclReport.some(b => comparator(a, b)));
 }
 
+
+function servicesToProperty(report) {
+  const mapper = new Map();
+  report.forEach(x => {
+    if (mapper.has(x.property)) {
+      mapper.get(x.property).push(...x.services);
+    } else {
+      mapper.set(x.property, x.services);
+    }
+  });
+  return mapper;
+}
+
+function uniqueBy(items, key) {
+  let seen = {};
+  return items.filter(function (item) {
+    const val = item[key];
+    return seen.hasOwnProperty(val) ? false : (seen[val] = true);
+  });
+}
+
 /**
  * @param {string} inputText JSON-LD object in expanded form
  * @return {Promise<Array<StructuredDataReport>>}
  */
-module.exports = async function validateSchemaOrg(inputText) {
-  inputText = prepareInput(inputText);
-  const report = await Promise.all(supportedServices.map(async service => {
-    const shexErrors = await shexValidator.validate(inputText, linkToShexShapes, defaultId, 'Recipe', service);
-    const shaclErrors = await shaclValidator.validate(inputText);
-    const serviceReport = findIntersection(shexErrors, shaclErrors);
-    serviceReport.forEach(x => x.service = service);
-    return serviceReport;
-  }));
+async function validateSinglePiece(inputText) {
+  const data = JSON.parse(inputText);
+  const dataType = Array.isArray(data['@type'])?data['@type'][0]:data['@type'];
+  const report = (await Promise.all(supportedServices.map(async service => {
+    const shexErrors = await shexValidator.validate(inputText, linkToShexShapes, data['@id'], dataType, service);
+    // const shaclErrors = await shaclValidator.validate(inputText);
+    // const serviceReport = findIntersection(shexErrors, shaclErrors);
+    shexErrors.forEach(x => {
+      x.services = [service];
+      x.shape = dataType;
+    });
+    return shexErrors;
+  }))).flat();
+  const propertyMap = servicesToProperty(report);
+  const finalReport = uniqueBy(report, 'property');
+  finalReport.forEach(x => x.services = propertyMap.get(x.property).join());
+  return finalReport;
+}
 
-  return report.flat();
+/**
+ * @param {Array<string>} inputText JSON-LD object in expanded form
+ * @return {Promise<Array<StructuredDataReport>>}
+ */
+module.exports = async function validateSchemaOrg(inputs) {
+  inputs = inputs.map(input => {
+    const data = JSON.parse(input);
+    if (data['@graph']) return data['@graph'].map(JSON.stringify);
+    return input;
+  }).flat();
+  const reports = await Promise.all(inputs.map(async inputText => {
+    inputText = prepareInput(inputText);
+    return await validateSinglePiece(inputText);
+  }));
+  return reports.flat();
 };
